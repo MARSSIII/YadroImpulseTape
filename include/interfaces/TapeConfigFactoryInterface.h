95b23a16a307413f7de51b9b5ded7b37935e8f4d
MARSSIII <satanicsevensins@gmail.com>
2025-05-05 02:54:31 +0300

    feat : add Interface for Config factory

---
+++ include/interfaces/TapeConfigFactoryInterface.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../entities/TapeConfig.h"
+
+class TapeConfigFactoryInterface {
+public:
+  virtual ~TapeConfigFactoryInterface() = default;
+  virtual TapeConfig create() const = 0;
+};
